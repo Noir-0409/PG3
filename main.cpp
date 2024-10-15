31556888da0d9168ba5ec0ff273f082222a1265b
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-10-15 12:26:06 +0900

    終了

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
-#include "stdio.h"
-#include "Windows.h"
+#include <stdio.h>
+#include <Windows.h>
+
+int Recursive(int hour) {
+    if (hour == 1) {
+        return 100; 
+    }
+    return Recursive(hour - 1) * 2 - 50;  
+}
+int FindProfitableHour() {
+    int standardTotalIncome = 0;
+    int recursiveTotalIncome = 0;
+    int hourCount = 0;
+
+  
+    while (recursiveTotalIncome <= standardTotalIncome) {
+        hourCount++;
+        standardTotalIncome += 1072;
+        recursiveTotalIncome += Recursive(hourCount);
+    }
+
+    return hourCount;
+}
 
 int main() {
+    SetConsoleOutputCP(65001); 
 
-SetConsoleOutputCP(65001);
+    int profitableHour = FindProfitableHour();
+    printf("–×‚©‚é‚Ì‚Í%dŽžŠÔŒã\n", profitableHour);
 
-return 0;
-
+    return 0;
 }
