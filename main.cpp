eb5808f89d64a4685f0b19b6af6dd5565c1a9e93
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-12-10 11:12:24 +0900

    終了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
-#include "stdio.h"
+#include <iostream>
+#include <string>
+#include <chrono>
 
 int main() {
 
-	printf("あああああ");
+    std::string a(100000, 'a');
 
-return 0;
+    auto start_time_move = std::chrono::high_resolution_clock::now();
+    std::string b = std::move(a);
+    auto end_time_move = std::chrono::high_resolution_clock::now();
+
+    a = std::string(100000, 'a');
+
+    auto start_time_copy = std::chrono::high_resolution_clock::now();
+    std::string c = a;
+    auto end_time_copy = std::chrono::high_resolution_clock::now();
+
+    auto time_move_duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time_move - start_time_move).count();
+    auto time_copy_duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time_copy - start_time_copy).count();
+
+    std::cout << "移動: " << time_move_duration << "マイクロ秒\n";
+    std::cout << "コピー: " << time_copy_duration << "マイクロ秒\n";
+
+    return 0;
 
 }
