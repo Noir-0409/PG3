c24ef5121b179c30256189970607c6a11409f473
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-01-20 10:35:31 +0900

    終了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+std::mutex mutex_;
+std::condition_variable condition_;
+int threadID = 1;
+
+void executeThread(int threadID) {
+
+    std::unique_lock<std::mutex> lock(mutex_);
+    condition_.wait(lock, [threadID]() { return threadID == threadID; });
+    std::cout << "thread" << threadID << std::endl;
+    threadID++;
+    condition_.notify_all();
+
+}
 
 int main() {
 
-	printf("あああああ");
+    std::thread thread1(executeThread, 1);
+    std::thread thread2(executeThread, 2);
+    std::thread thread3(executeThread, 3);
 
-return 0;
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    return 0;
 
 }
